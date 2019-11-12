a1132f9dea0be9facaee626904e587f8c042494a
miltonferreira <jotonferreira@gmail.com>
2019-11-12 17:17:21 -0300

    acrescentando comentários ao Post

--- src/main/java/com/jotonferreira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jotonferreira/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.jotonferreira.workshopmongo.domain.Post;
 import com.jotonferreira.workshopmongo.domain.User;
 import com.jotonferreira.workshopmongo.dto.AuthorDTO;
+import com.jotonferreira.workshopmongo.dto.CommentDTO;
 import com.jotonferreira.workshopmongo.repository.PostRepository;
 import com.jotonferreira.workshopmongo.repository.UserRepository;
 
@@ -47,6 +48,17 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria)); // com new AuthorDTO se tem um obj para pegar as infos
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz hoje!",  new AuthorDTO(maria)); // com new AuthorDTO se tem um obj para pegar as infos
+		
+		// comments --------------------------------------------------------
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2)); // salva comentarios no MongoDB atrelado ao post
+		post2.getComments().addAll(Arrays.asList(c3)); // salva comentarios no MongoDB atrelado ao post
+		
+		// comments --------------------------------------------------------
 		
 		postRepository.saveAll(Arrays.asList(post1, post2)); // salva post's no MongoDB
 		// Post ------------------------------------------------------------------------------------
--- src/main/java/com/jotonferreira/workshopmongo/domain/Post.java
+++ src/main/java/com/jotonferreira/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.jotonferreira.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.jotonferreira.workshopmongo.dto.AuthorDTO;
+import com.jotonferreira.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,6 +22,9 @@ public class Post implements Serializable{
 	private String title;
 	private String body;  // mensagem do post
 	private AuthorDTO author; // pega um AuthorDTO para escolher somente as infos relevantes de User
+	
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {}
 
@@ -69,6 +75,14 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
